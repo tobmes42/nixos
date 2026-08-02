1240d803654b416ea3e6e100b1b37c97877e0ec1
tobmes42 <58036175+tobmes42@users.noreply.github.com>
2026-08-02 19:01:40 +0200

    Add initial disko.nix configuration for disk setup

---
+++ disko.nix
@@ -0,0 +1,39 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/sda";
+
+        content = {
+          type = "gpt";
+
+          partitions = {
+            ESP = {
+              priority = 1;
+              size = "512M";
+              type = "EF00";
+
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+
+            root = {
+              priority = 2;
+              size = "100%";
+
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
