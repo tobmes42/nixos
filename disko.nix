f76de84d8296d45680829346463700497a8674c6
tobmes42 <58036175+tobmes42@users.noreply.github.com>
2026-08-02 20:12:18 +0200

    Add disk configuration with GPT and partitions

--- disko.nix
+++ disko.nix
@@ -1,39 +1,70 @@
 {
   disko.devices = {
+
     disk = {
+
       main = {
+
         type = "disk";
+
         device = "/dev/sda";
 
+
         content = {
+
           type = "gpt";
 
+
           partitions = {
+
             ESP = {
+
               priority = 1;
+
               size = "512M";
+
               type = "EF00";
 
+
               content = {
+
                 type = "filesystem";
+
                 format = "vfat";
+
                 mountpoint = "/boot";
+
               };
+
             };
+
 
             root = {
+
               priority = 2;
+
               size = "100%";
 
+
               content = {
+
                 type = "filesystem";
+
                 format = "ext4";
+
                 mountpoint = "/";
+
               };
+
             };
+
           };
+
         };
+
       };
+
     };
+
   };
 }
