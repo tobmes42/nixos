545fc26564b24cbe5222ac53cdfd6a2bda839647
tobmes <tobias.messinger.ext@bwi.de>
2026-08-04 05:27:30 +0000

    feat(vscode): enable VS Code Server and add to NixOS configuration

--- flake.nix
+++ flake.nix
@@ -6,10 +6,12 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
 
-  outputs = { self, nixpkgs, disko, ... }:
+  outputs = { self, nixpkgs, disko, vscode-server, ... }:
   {
     nixosConfigurations.server-001 = nixpkgs.lib.nixosSystem {
 
@@ -19,6 +21,7 @@
         ./configuration.nix
         ./hardware-configuration.nix
         disko.nixosModules.disko
+        vscode-server.nixosModules.default
       ] ++ nixpkgs.lib.optionals (builtins.pathExists ./network.nix) [
         ./network.nix
       ] ++ nixpkgs.lib.optionals (builtins.pathExists ./hostname.nix) [
